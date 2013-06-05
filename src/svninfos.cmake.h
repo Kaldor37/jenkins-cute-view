b551ba30e0be20eb170eaa79f9fcd43c2f222da7
Davy Gabard <davy.gabard@gmail.com>
2013-06-05 15:01:59 +0000

    Added SVN infos header file

---
+++ src/svninfos.cmake.h
@@ -0,0 +1,38 @@
+//------------------------------------------------------------------------------
+#ifndef SVNINFOS_H
+#define SVNINFOS_H
+//------------------------------------------------------------------------------
+// SVN informations header (filled by CMake)
+//------------------------------------------------------------------------------
+/**
+ * Url of the repository
+ */
+#define SVN_WC_URL	"${SVN_WC_URL}"
+
+/**
+ * Root url of the repository
+ */
+#define SVN_WC_ROOT "${SVN_WC_ROOT}"
+
+/**
+ * Current revision
+ */
+#define SVN_WC_REVISION "${SVN_WC_REVISION}"
+
+/**
+ * Date of last commit
+ */
+#define SVN_WC_LAST_CHANGED_DATE "${SVN_WC_LAST_CHANGED_DATE}"
+
+/**
+ * Revision of last commit
+ */
+#define SVN_WC_LAST_CHANGED_REV "${SVN_WC_LAST_CHANGED_REV}"
+
+/**
+ * Last log of base revision
+ */
+#define SVN_WC_LAST_CHANGED_LOG "${SVN_WC_LAST_CHANGED_LOG}"
+//------------------------------------------------------------------------------
+#endif // SVNINFOS_H
+//------------------------------------------------------------------------------
